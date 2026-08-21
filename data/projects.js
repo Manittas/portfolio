de4f56ecab88a6dd59fcafe2c5477b6654745488
Filipe Neves <66378018+Manittas@users.noreply.github.com>
2026-08-21 18:44:06 +0100

    create default files for data and vinyls.html

---
+++ data/projects.js
@@ -0,0 +1,20 @@
+const projects = [
+    {
+        name: "Crochet Stitch Counter App",
+        description: "Python desktop application, using Tkinter, that counts crochet stitches across multiple rows. Builder compatible with Windows and macOS.",
+        technologies: ["Python", "Tkinter"],
+        github: "https://github.com/Manittas/Crochet-Stitch-Count-App"
+    },
+    {
+        name: "LSB Extraction Tool",
+        description: "Python script focused on extracting files and messages encripted through LSB encoding into RGB pixels of images.",
+        technologies: ["Python"],
+        github: "https://github.com/Manittas/LSB-Extraction-Script"
+    },
+    {
+        name: "Discord Bot",
+        description: "Python script for a discord bot that sends quotes through commands, plays text-to-speech quotes in voice channels and notifies when a member enters a voice channel.",
+        technologies: ["Python", "discord.py"],
+        github: "https://github.com/Manittas/Bot-Script-for-Discord"
+    }
+];
---
+++ data/vinyl.js
@@ -0,0 +1,8 @@
+const vinyls = [
+    {
+        album: "...",
+        artist: "...",
+        cover: "...",
+        preview: "..."
+    }
+];
